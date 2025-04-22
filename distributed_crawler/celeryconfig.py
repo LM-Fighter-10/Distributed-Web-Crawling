f0be3211942c8defa1b8c6b3a220e66f0d46fede
Omar Alaa Elnahas <21p0197@eng.asu.edu.eg>
2025-04-22 00:49:40 +0000

    add: crawler communication with indexer node

--- distributed_crawler/celeryconfig.py
+++ distributed_crawler/celeryconfig.py
@@ -1,13 +1,18 @@
 # celeryconfig.py
+
 from kombu import Exchange, Queue
 
+# Redis broker/results on your crawler VM
 broker_url = 'redis://10.128.0.2:6379/0'
 result_backend = 'redis://10.128.0.2:6379/1'
 
+# single queue for crawling jobs
+crawl_exchange = Exchange('crawl', type='direct')
 task_queues = (
-    Queue('crawl_tasks', Exchange('crawl'), routing_key='crawl.url'),
+    Queue('crawl_tasks', crawl_exchange, routing_key='crawl.url'),
 )
 
+# route our crawl_url task into that queue
 task_routes = {
     'tasks.crawl_url': {'queue': 'crawl_tasks', 'routing_key': 'crawl.url'},
 }
